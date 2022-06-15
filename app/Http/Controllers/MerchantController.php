094d447b5cbb08254e6c727de035a0cee9753832
Ganjar Hadiatna <ganjardbc@gmail.com>
2022-06-15 17:46:04 +0700

    add merchant api

---
+++ app/Http/Controllers/MerchantController.php
@@ -0,0 +1,452 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Merchant;
+use Image;
+
+class MerchantController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+
+    public function getAll(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'limit' => 'required|integer',
+            'offset' => 'required|integer',
+            'status' => 'string'
+        ]);
+
+        $response = [];
+
+        if ($validator->fails()) 
+        {
+            $response = [
+                'message' => $validator->errors(),
+                'status' => 'invalide',
+                'code' => '201',
+                'data' => []
+            ];
+        } 
+        else 
+        {
+            $status = $req['status'];
+            $limit = $req['limit'];
+            $offset = $req['offset'];
+            $stt = $status ? ['status' => $status] : [];
+            $data = Merchant::where($stt)->limit($limit)->offset($offset)->orderBy('id', 'desc')->get();
+            if ($data) 
+            {
+                $response = [
+                    'message' => 'proceed success',
+                    'status' => 'ok',
+                    'code' => '201',
+                    'data' => $data
+                ];
+            } 
+            else 
+            {
+                $response = [
+                    'message' => 'failed to get datas',
+                    'status' => 'failed',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+        }
+
+        return response()->json($response, 200);
+    }
+
+    public function getByID(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'merchant_id' => 'required|string|min:0|max:17',
+        ]);
+
+        $response = [];
+
+        if ($validator->fails()) 
+        {
+            $response = [
+                'message' => $validator->errors(),
+                'status' => 'invalide',
+                'code' => '201',
+                'data' => []
+            ];
+        } 
+        else 
+        {
+            $merchant_id = $req['merchant_id'];
+            $data = Merchant::where(['merchant_id' => $merchant_id])->first();
+            if ($data) 
+            {
+                $response = [
+                    'message' => 'proceed success',
+                    'status' => 'ok',
+                    'code' => '201',
+                    'data' => $data
+                ];
+            } 
+            else 
+            {
+                $response = [
+                    'message' => 'failed to get datas',
+                    'status' => 'failed',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+        }
+
+        return response()->json($response, 200);
+    }
+
+    public function removeImage(Request $req) 
+    {
+        $validator = Validator::make($req->all(), [
+            'merchant_id' => 'required|string|min:0|max:17',
+        ]);
+        
+        $response = [];
+
+        if ($validator->fails()) 
+        {
+            $response = [
+                'message' => $validator->errors(),
+                'status' => 'invalide',
+                'code' => '201',
+                'data' => []
+            ];
+        } 
+        else 
+        {
+            $payload = [
+                'image' => '',
+                'updated_by' => Auth()->user()->id,
+                'updated_at' => date('Y-m-d H:i:s')
+            ];
+
+            $filename = Merchant::where(['merchant_id' => $req['merchant_id']])->first()->image;
+            $data = Merchant::where(['merchant_id' => $req['merchant_id']])->update($payload);
+
+            if ($data)
+            {
+                unlink(public_path('contents/merchants/thumbnails/'.$filename));
+				unlink(public_path('contents/merchants/covers/'.$filename));
+
+                $response = [
+                    'message' => 'proceed success',
+                    'status' => 'ok',
+                    'code' => '201',
+                    'data' => Merchant::where(['merchant_id' => $req['merchant_id']])->first()
+                ];
+            }
+            else 
+            {
+                $response = [
+                    'message' => 'failed to remove image',
+                    'status' => 'failed',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+        }
+    }
+
+    public function uploadImage(Request $req) 
+    {
+        $validator = Validator::make($req->all(), [
+            'merchant_id' => 'required|string|min:0|max:17',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:1000000'
+        ]);
+
+        $response = [];
+
+        if ($validator->fails()) 
+        {
+            $response = [
+                'message' => $validator->errors(),
+                'status' => 'invalide',
+                'code' => '201',
+                'data' => []
+            ];
+        } 
+        else 
+        {
+            $id = $req['merchant_id'];
+            $image = $req['image'];
+
+            $chrc = array('[',']','@',' ','+','-','#','*','<','>','_','(',')',';',',','&','%','$','!','`','~','=','{','}','/',':','?','"',"'",'^');
+			$filename = $id.time().str_replace($chrc, '', $image->getClientOriginalName());
+			$width = getimagesize($image)[0];
+			$height = getimagesize($image)[1];
+
+            //save image to server
+			//creating thumbnail and save to server
+			$destination = public_path('contents/merchants/thumbnails/'.$filename);
+			$img = Image::make($image->getRealPath());
+			$thumbnail = $img->resize(400, 400, function ($constraint) {
+					$constraint->aspectRatio();
+				})->save($destination); 
+
+			//saving image real to server
+			$destination = public_path('contents/merchants/covers/');
+			$real = $image->move($destination, $filename);
+
+            if ($thumbnail && $real) 
+			{
+                $payload = [
+                    'image' => $filename,
+                    'updated_by' => Auth()->user()->id,
+                    'updated_at' => date('Y-m-d H:i:s')
+                ];
+    
+                $data = Merchant::where(['merchant_id' => $req['merchant_id']])->update($payload);
+    
+                if ($data)
+                {
+                    $response = [
+                        'message' => 'proceed success',
+                        'status' => 'ok',
+                        'code' => '201',
+                        'data' => Merchant::where(['merchant_id' => $req['merchant_id']])->first()
+                    ];
+                }
+                else 
+                {
+                    $response = [
+                        'message' => 'failed to save',
+                        'status' => 'failed',
+                        'code' => '201',
+                        'data' => []
+                    ];
+                }
+            }
+            else 
+            {
+                $response = [
+                    'message' => 'failed to upload image',
+                    'status' => 'failed',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+        }
+
+        return response()->json($response, 200);
+    }
+
+    public function post(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'merchant_id' => 'required|string|min:0|max:17|unique:merchants',
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'type' => 'required|string',
+            'is_available' => 'required|integer',
+            'status' => 'required|string'
+        ]);
+
+        $response = [];
+
+        if ($validator->fails()) 
+        {
+            $response = [
+                'message' => $validator->errors(),
+                'status' => 'invalide',
+                'code' => '201',
+                'data' => []
+            ];
+        } 
+        else 
+        {
+            $payload = [
+                'merchant_id' => $req['merchant_id'],
+                'name' => $req['name'],
+                'description' => $req['description'],
+                'type' => $req['type'],
+                'is_available' => $req['is_available'],
+                'status' => $req['status'],
+                'created_by' => Auth()->user()->id,
+                'created_at' => date('Y-m-d H:i:s')
+            ];
+
+            $data = Merchant::insert($payload);
+
+            if ($data)
+            {
+                $response = [
+                    'message' => 'proceed success',
+                    'status' => 'ok',
+                    'code' => '201',
+                    'data' => Merchant::where(['merchant_id' => $req['merchant_id']])->first()
+                ];
+            }
+            else 
+            {
+                $response = [
+                    'message' => 'failed to save',
+                    'status' => 'failed',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+        }
+
+        return response()->json($response, 200);
+    }
+
+    public function update(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'merchant_id' => 'required|string|min:0|max:17',
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'type' => 'required|string',
+            'is_available' => 'required|integer',
+            'status' => 'required|string'
+        ]);
+
+        $response = [];
+
+        if ($validator->fails()) 
+        {
+            $response = [
+                'message' => $validator->errors(),
+                'status' => 'invalide',
+                'code' => '201',
+                'data' => []
+            ];
+        } 
+        else 
+        {
+            $payload = [
+                'name' => $req['name'],
+                'description' => $req['description'],
+                'type' => $req['type'],
+                'is_available' => $req['is_available'],
+                'status' => $req['status'],
+                'updated_by' => Auth()->user()->id,
+                'updated_at' => date('Y-m-d H:i:s')
+            ];
+
+            $data = Merchant::where(['merchant_id' => $req['merchant_id']])->update($payload);
+
+            if ($data)
+            {
+                $response = [
+                    'message' => 'proceed success',
+                    'status' => 'ok',
+                    'code' => '201',
+                    'data' => Merchant::where(['merchant_id' => $req['merchant_id']])->first()
+                ];
+            }
+            else 
+            {
+                $response = [
+                    'message' => 'failed to save',
+                    'status' => 'failed',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+        }
+
+        return response()->json($response, 200);
+    }
+
+    public function delete(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'merchant_id' => 'required|string|min:0|max:17',
+        ]);
+
+        $response = [];
+
+        if ($validator->fails()) 
+        {
+            $response = [
+                'message' => $validator->errors(),
+                'status' => 'invalide',
+                'code' => '201',
+                'data' => []
+            ];
+        } 
+        else 
+        {
+            $data = Merchant::where(['merchant_id' => $req['merchant_id']])->delete();
+
+            if ($data)
+            {
+                $response = [
+                    'message' => 'proceed success',
+                    'status' => 'ok',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+            else 
+            {
+                $response = [
+                    'message' => 'failed to delete',
+                    'status' => 'failed',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+        }
+
+        return response()->json($response, 200);
+    }
+
+    public function exit(Request $req)
+    {
+        $validator = Validator::make($req->all(), [
+            'owner_id' => 'required|integer',
+        ]);
+
+        $response = [];
+
+        if ($validator->fails()) 
+        {
+            $response = [
+                'message' => $validator->errors(),
+                'status' => 'invalide',
+                'code' => '201',
+                'data' => []
+            ];
+        } 
+        else 
+        {
+            $cart = Cart::where(['owner_id' => $req['owner_id']])->delete();
+            $wh = WisheList::where(['owner_id' => $req['owner_id']])->delete();
+
+            if ($cart || $wh)
+            {
+                $response = [
+                    'message' => 'proceed success',
+                    'status' => 'ok',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+            else 
+            {
+                $response = [
+                    'message' => 'failed to delete',
+                    'status' => 'failed',
+                    'code' => '201',
+                    'data' => []
+                ];
+            }
+        }
+
+        return response()->json($response, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -29,9 +29,59 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('getAll', 'AdminController@getAll');
         Route::get('getByID', 'AdminController@getByID');
         Route::post('changePassword', 'AdminController@changePassword');
+        Route::post('uploadImage', 'AdminController@uploadImage');
+        Route::post('removeImage', 'AdminController@removeImage');
         Route::post('post', 'AdminController@post');
         Route::put('update', 'AdminController@update');
         Route::delete('delete', 'AdminController@delete');
+    });
+
+    // bizpars
+    Route::prefix('bizpar')->group(function () {
+        Route::get('getAll', 'BizparController@getAll');
+        Route::get('getByType', 'BizparController@getByType');
+        Route::get('getByKey', 'BizparController@getByKey');
+        Route::post('post', 'BizparController@post');
+        Route::put('update', 'BizparController@update');
+        Route::delete('delete', 'BizparController@delete');
+    });
+
+    // role
+    Route::prefix('role')->group(function () {
+        Route::get('getAll', 'RoleController@getAll');
+        Route::get('getByID', 'RoleController@getByID');
+        Route::post('post', 'RoleController@post');
+        Route::put('update', 'RoleController@update');
+        Route::delete('delete', 'RoleController@delete');
+    });
+
+    // permission
+    Route::prefix('permission')->group(function () {
+        Route::get('getAll', 'PermissionController@getAll');
+        Route::get('getByID', 'PermissionController@getByID');
+        Route::post('post', 'PermissionController@post');
+        Route::put('update', 'PermissionController@update');
+        Route::delete('delete', 'PermissionController@delete');
+    });
+
+    // role permission
+    Route::prefix('rolepermission')->group(function () {
+        Route::get('getAll', 'RolePermissionController@getAll');
+        Route::post('checkRolePermission', 'RolePermissionController@checkRolePermission');
+        Route::post('post', 'RolePermissionController@post');
+        Route::put('update', 'RolePermissionController@update');
+        Route::delete('delete', 'RolePermissionController@delete');
+    });
+
+    // merchant
+    Route::prefix('merchant')->group(function () {
+        Route::get('getAll', 'MerchantController@getAll');
+        Route::get('getByID', 'MerchantController@getByID');
+        Route::post('uploadImage', 'MerchantController@uploadImage');
+        Route::post('removeImage', 'MerchantController@removeImage');
+        Route::post('post', 'MerchantController@post');
+        Route::put('update', 'MerchantController@update');
+        Route::delete('delete', 'MerchantController@delete');
     });
 });
 
